cb359361c03b489a0e20a726b36d8055525ae539
solareenlo <solareenlo@gmail.com>
2021-11-14 16:25:28 +0900

    Fix: ex00

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 10:03:51 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/11/14 16:19:45 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/11/14 16:25:08 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ ClapTrap::ClapTrap(ClapTrap const& src) {
 ClapTrap& ClapTrap::operator=(ClapTrap const& rhs) {
     if (this != &rhs) {
         std::cout << "ClapTrap " << this->getName() << " has been replaced by "
-                  << "ClapTrap " << rhs.getName() << std::endl;
+                  << "ClapTrap " << rhs.getName() << "." << std::endl;
         this->setName(rhs.getName());
         this->setHitPoints(rhs.getHitPoints());
         this->setMaxHitPoints(rhs.getMaxHitPoints());
